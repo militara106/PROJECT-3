e50b44655a66d2ab261a0eb6bad3314b81505419
Alex <militara106@gmail.com>
2019-11-12 20:51:23 -0800

    moved audio player to bottom of page

--- client2/src/App/components/Navbar/index.js
+++ client2/src/App/components/Navbar/index.js
@@ -1,17 +1,11 @@
 import React from "react";
-import ReactAudioPlayer from 'react-audio-player';
 
 function Navbar() {
   return (
     <div>
     <nav className="navbar navGradient lightFont navbar-fixed-top topNavBar">
         <h1 className="vybe">VYBE</h1>
-        <div>
-        <ReactAudioPlayer
-  src="bensound-creativeminds.mp3"
-  autoPlay
-  controls
-/></div> 
+        <div>Options/Filler</div> 
     </nav>
     <div className="lowerBorderGradient" />
     </div>
--- client2/src/App/pages/Home.js
+++ client2/src/App/pages/Home.js
@@ -4,9 +4,15 @@ import Col from "../components/Col";
 import NavBar from "../components/Navbar";
 import TextOverlay from "../components/TextOverlay";
 import MainVisual from "../components/MainVisual";
-
+import ReactAudioPlayer from "react-audio-player";
 
 // import { Link } from 'react-router-dom';
+
+const audioPos = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}
 
 class Home extends Component {
   render() {
@@ -15,6 +21,9 @@ class Home extends Component {
         <NavBar />
         <MainVisual />
         <TextOverlay songName="Song Name" artist="Artist" />
+        <div style={audioPos}>
+          <ReactAudioPlayer src="bensound-creativeminds.mp3" autoPlay controls />
+        </div>
       </div>
     );
   }

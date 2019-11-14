711c12d3b06e99bfa2029acda1baa9febc9d01a8
tylerhipke <tylerhipke@gmail.com>
2019-11-14 11:07:46 -0800

    add spotify login

--- client2/src/App/App.js
+++ client2/src/App/App.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-<<<<<<< HEAD
-import List from './pages/List';
-=======
->>>>>>> 5aee040b60e146f9465e0e3d76c8e69961a91326
-import Dashboard from './pages/Dashboard'
+import Dashboard from './pages/Dashboard';
+import SpotifyAuth from './pages/SpotifyAuth';
 
 class App extends Component {
   render() {
@@ -14,10 +11,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' component={Home}/>
-<<<<<<< HEAD
-          <Route path='/list' component={List}/>
-=======
->>>>>>> 5aee040b60e146f9465e0e3d76c8e69961a91326
+          <Route exact path='/spotify' component={SpotifyAuth}/>
           <Route path='/dashboard' component={Dashboard}/>
         </Switch>
       </div>
---
+++ server.js
@@ -0,0 +1,22 @@
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app
+  .use(express.static(__dirname + "/public"));
+
+// Serve the static files from the React app
+
+app.use(express.static(path.join(__dirname, "client2/build")))
+   .use(cors())
+   .use(cookieParser());
+
+require("./client2/src/App/components/utils/Api.js")(app);
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log("App is listening on port " + port);

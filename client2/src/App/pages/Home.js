ff9afd8a49bd1fe8d46ec6c6f53cb2c795dcfcaa
Alex <militara106@gmail.com>
2019-11-16 11:27:16 -0800

    visualizer temp toggle created and error fixing

--- client2/src/App/pages/Home.js
+++ client2/src/App/pages/Home.js
@@ -21,139 +21,115 @@ class Home extends Component {
     artist: "Bensound",
     src: "bensound-creativeminds.mp3",
     account: "",
-    room: ""
+    room: "",
+    mediaElement: "",
+    visualizerCheck: false
   };
 
+  clicky = () => {
+    // Audio Variables
+    let audio = document.getElementById("audio-element");
+    // let audioContext = new AudioContext();
+    let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    let src = this.state.mediaElement;
+    console.log("Audio: " + audio.src);
+
+    // Analyzer Variables
+    let analyzer = audioContext.createAnalyser();
+    let freqArray = new Uint8Array(analyzer.frequencyBinCount);
+    analyzer.connect(audioContext.destination);
+    analyzer.fftSize = 512;
+
+    // Toggle Logic
+    let canvas = document.getElementById("visualizer");
+    if(this.state.visualizerCheck  === false){
+      canvas.style.display = "block";
+      this.setState({visualizerCheck: true});
+    }
+    else{
+      this.setState({visualizerCheck:false});
+      canvas.style.display = "none";
+    }
+
+
+    if(this.state.mediaElement === ""){
+      src = audioContext.createMediaElementSource(audio);
+      src.connect(analyzer);
+      this.setState({mediaElement:src});
+      this.Visualizer(analyzer, freqArray);
+      }
+
+
+  }
 
   // ----------------------AUDIO VISUALIZER-----------------------
-  Visualizer = () => {
+  Visualizer = (analyzer, freqArray) => {
     // Canvas Variables
     let canvas = document.getElementById("visualizer");
     let canvasContext = canvas.getContext("2d");
     canvas.width = document.getElementById("visualizerContainer").offsetWidth;
     canvas.height = document.getElementById("visualizerContainer").offsetHeight;
     console.log("Width: " + canvas.width + " Height: " + canvas.height);
-  
+
     //  Canvas Background Color
     let fillStyle = "rgba(0,0,0,0)";
-  
-    // Audio Variables
-    let audio = document.getElementById("audio-element");
-    audio.load();
-  
-    //  let audioContext = new AudioContext();
-    let audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    let src = audioContext.createMediaElementSource(audio);
-    console.log("Audio: " + audio.src);
-  
-    // Analyzer Variables
-    let analyzer = audioContext.createAnalyser();
-    let freqArray = new Uint8Array(analyzer.frequencyBinCount);
-    src.connect(analyzer);
-    analyzer.connect(audioContext.destination);
-    analyzer.fftSize = 512;
-  
+
     // Visualizer Variables
     let barHeight = 5;
     let barWidth = (canvas.width / analyzer.frequencyBinCount) * 2.5;
-    let x,y,x2,y2;
+    // let x,y,x2,y2;
     let g, b;
     let r = 0;
-  
-    // renderBarVisualizer();
-    // function renderBarVisualizer() {
-    //   // Base Canvas
-    //   canvasContext.fillStyle = fillStyle;
-    //   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  
-    //   //Bar Variables
-    //   requestAnimationFrame(renderBarVisualizer);
-    //   analyzer.getByteFrequencyData(freqArray);
-    //   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  
-    //   x = canvas.width / 2;
-    //   x2 = x;
-  
-    //   // Draw Bars
-    //   for (var i = 0; i < analyzer.frequencyBinCount; i++) {
-    //     barHeight = freqArray[i] + 5;
-  
-    //     // Setting Colors
-    //     b = barHeight + 25 * (i / analyzer.frequencyBinCount) + 50;
-    //     g = barHeight + 25 * (i / analyzer.frequencyBinCount) + 50;
-    //     r = 0;
-    //     canvasContext.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-    //     canvasContext.strokeStyle = "rgb(" + r + "," + g + "," + b + ")";
-  
-    //     canvasContext.fillRect(
-    //       x,
-    //       (canvas.height - barHeight * 2.5) / 2,
-    //       barWidth,
-    //       barHeight * 2.5
-    //     );
-    //     canvasContext.fillRect(
-    //       x2,
-    //       (canvas.height - barHeight * 2.5) / 2,
-    //       barWidth,
-    //       barHeight * 2.5
-    //     );
-  
-    //     x += barWidth + 1;
-    //     x2 -= barWidth + 1;
-    //   }
-    // }
 
     renderRoundVisualizer();
     function renderRoundVisualizer() {
-
       requestAnimationFrame(renderRoundVisualizer);
       analyzer.getByteFrequencyData(freqArray);
 
       var center_x = canvas.width / 2;
       var center_y = canvas.height / 2;
       var radius = 1;
-      var circles = 50;
+      var circles = 30;
 
       canvasContext.clearRect(0, 0, canvas.width, canvas.height);
       drawBaseCanvas();
       drawArc(center_x, center_y, radius, 0, 2 * Math.PI);
 
       for (var i = 0; i < circles; i++) {
-          barHeight = freqArray[i];
-          radius = freqArray[i];
+        barHeight = freqArray[i];
+        radius = freqArray[i];
 
-          setCanvasColor(i,barHeight);
-          canvasContext.lineWidth = barWidth/5;
-          drawArc(center_x, center_y, radius + i * 20, i, 2 * Math.PI - i);
-
-
+        setCanvasColor(i, barHeight);
+        canvasContext.lineWidth = barWidth / 5;
+        drawArc(center_x, center_y, radius + i * 20, i, 2 * Math.PI - i);
       }
-  }
+    }
 
     //  Draw Base Canvas
-    function drawBaseCanvas(){
+    function drawBaseCanvas() {
       canvasContext.fillStyle = fillStyle;
       canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-   }
+    }
 
-  //  Set Canvas color
-   function setCanvasColor(i,barHeight){
-    b = barHeight + 25 * (i / analyzer.frequencyBinCount) + 50;
-        g = barHeight + 25 * (i / analyzer.frequencyBinCount) + 50;
-        r = 0;
-        canvasContext.fillStyle = "rgba(" + r + "," + g + "," + b + "," + 0.5 + ")";
-        canvasContext.strokeStyle = "rgba(" + r + "," + g + "," + b + "," + 0.5 + ")";
-   }
+    //  Set Canvas color
+    function setCanvasColor(i, barHeight) {
+      b = barHeight + 25 * (i / analyzer.frequencyBinCount) + 50;
+      g = barHeight + 25 * (i / analyzer.frequencyBinCount) + 50;
+      r = 0;
+      canvasContext.fillStyle =
+        "rgba(" + r + "," + g + "," + b + "," + 0.5 + ")";
+      canvasContext.strokeStyle =
+        "rgba(" + r + "," + g + "," + b + "," + 0.5 + ")";
+    }
 
-  //  Draw Arc
-   function drawArc(x,y,radius,start,end){
+    //  Draw Arc
+    function drawArc(x, y, radius, start, end) {
       canvasContext.beginPath();
-      canvasContext.arc(x,y,radius,start,end);
+      canvasContext.arc(x, y, radius, start, end);
       canvasContext.stroke();
-   }
-  }
+    }
+  };
 
-  
   // ----------------------AUDIO VISUALIZER END-----------------------
 
   render() {
@@ -166,7 +142,7 @@ class Home extends Component {
         <MainVisual>
           {/* <div className="fillerText">Visualizer Center</div> */}
           <div id="visualizerContainer">
-          <canvas id="visualizer"></canvas>
+            <canvas id="visualizer"></canvas>
           </div>
         </MainVisual>
 
@@ -183,7 +159,7 @@ class Home extends Component {
             id={"audio-element"}
             controls
           />
-          <div onClick={this.Visualizer}>Visualizer Button</div>
+          <div onClick={this.clicky}>Click</div>
         </div>
       </div>
     );

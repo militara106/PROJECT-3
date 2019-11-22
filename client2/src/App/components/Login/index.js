b99552c2c1e24ac87a9687b130f1c93b23133fe4
tylerhipke <tylerhipke@gmail.com>
2019-11-22 12:50:43 -0800

    add spotify connect and remote control scopes

--- client2/src/App/components/Login/index.js
+++ client2/src/App/components/Login/index.js
@@ -1,22 +1,24 @@
 export default {
-    logInWithSpotify: (() => {
+  logInWithSpotify: () => {
+    var clientId = "198e199ecc1640cfb59d3ab919efd393";
+    var callback = "http://bcs-project-3.herokuapp.com/dashboard";
+    var scopes = [
+      "streaming",
+      "user-read-email",
+      "user-read-private",
+      "user-modify-playback-state",
+      "user-read-playback-state",
+      "user-read-currently-playing",
+      "app-remote-control"
+    ];
 
-      var clientId =  "198e199ecc1640cfb59d3ab919efd393";
-      var callback =  "http://bcs-project-3.herokuapp.com/dashboard";
-      var scopes = [
-        "streaming",
-        "user-read-birthdate",
-        "user-read-email",
-        "user-read-private"
-      ];
-
-      window.location = [
-        "https://accounts.spotify.com/authorize",
-        `?client_id=${clientId}`,
-        `&redirect_uri=${callback}`,
-        `&scope=${scopes}`,
-        "&response_type=token",
-        "&show_dialog=true"
-      ].join('');
-    })
-  };
+    window.location = [
+      "https://accounts.spotify.com/authorize",
+      `?client_id=${clientId}`,
+      `&redirect_uri=${callback}`,
+      `&scope=${scopes}`,
+      "&response_type=token",
+      "&show_dialog=true"
+    ].join("");
+  }
+};

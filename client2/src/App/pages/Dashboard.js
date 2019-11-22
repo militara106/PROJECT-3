9c1f5f704fe583d9d0b74a9b0759bdb9ab8c34e2
tylerhipke <tylerhipke@gmail.com>
2019-11-21 19:52:58 -0800

    initial complete rewrite of spotify api

--- client2/src/App/pages/Dashboard.js
+++ client2/src/App/pages/Dashboard.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import SpotifyAuth from '../components/SpotifyAuth';
-import SpotifyPlayer from '../components/SpotifyPlayer'
-
 
 class Dashboard extends Component {
   render() {
     return (
     <div className="App">
       <h1>Spotify Dashboard</h1>
-      < SpotifyPlayer />
     </div>
     );
   }

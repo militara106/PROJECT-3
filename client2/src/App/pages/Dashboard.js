742d01f2e876d9c6f8bc50ea47ef4682c814afb4
tylerhipke <tylerhipke@gmail.com>
2019-11-21 20:28:38 -0800

    remove old spotify api

--- client2/src/App/pages/Dashboard.js
+++ client2/src/App/pages/Dashboard.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import SpotifyAuth from '../components/SpotifyAuth';
-
 
 class Dashboard extends Component {
   render() {
     return (
     <div className="App">
       <h1>Spotify Dashboard</h1>
-      < SpotifyAuth />
     </div>
     );
   }
--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+require("dotenv").config();
+
+app.use(express.static(__dirname + "/public"));
+
 app
-  .use(express.static(__dirname + "/public"));
+  .use(express.static(path.join(__dirname, "client2/build")))
+  .use(cors())
+  .use(cookieParser());
 
-// Serve the static files from the React app
-
-app.use(express.static(path.join(__dirname, "client2/build")))
-   .use(cors())
-   .use(cookieParser());
-
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname+'/client2/build/index.html'))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client2/build/index.html"));
 });
-
-// require("./client2/src/App/utils/API")(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log("App is listening on port " + port);
+

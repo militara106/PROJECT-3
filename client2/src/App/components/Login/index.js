962a23c86732e73e6106415f66f0a3d85a0d7172
tylerhipke <tylerhipke@gmail.com>
2019-11-23 11:29:27 -0800

    change callback url

--- client2/src/App/components/Login/index.js
+++ client2/src/App/components/Login/index.js
@@ -1,7 +1,7 @@
 export default {
   logInWithSpotify: () => {
     var clientId = "198e199ecc1640cfb59d3ab919efd393";
-    var callback = "http://localhost:5000/home";
+    var callback = "http://bcs-project-3.heroku.com/home";
     var scopes = [
       "streaming",
       "user-read-email",

6a1541e64de74521956619afc2a2947952f94a7a
Alex <militara106@gmail.com>
2019-11-26 19:40:48 -0800

    Login auth0 added

--- client2/src/App/pages/Login.js
+++ client2/src/App/pages/Login.js
@@ -1,33 +1,43 @@
 import React, { Component } from "react";
 import Row from "../components/Row";
 import Col from "../components/Col";
-class Login extends Component {
-  // Default State/Song
-  state = {
-    songName: "Creative",
-    artist: "Bensound",
-    src: "bensound-creativeminds.mp3",
-    account: "",
-    room: ""
-  };
+import { useAuth0 } from "../../react-auth0-spa";
 
-  render() {
-    return (
-      <div className="d-flex flex-column h-100">
-        <div className="loginBG" />
-        <h1 className="vybe text-center">VYBE</h1>
-        <div className="container h-100">
-          <Row className=" h-100 row align-items-center">
-            <Col size="sm-7">
-              <h3 className="testColor">Intro</h3>
-            </Col>
-            <Col size="sm-5">
-              <h3 className="testColor">Login Block</h3>
-            </Col>
-          </Row>
-        </div>
-      </div>
-    );
+const Login = () => {
+  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  const logoutWithRedirect = () => {
+    logout({
+      returnTo: window.location.origin
+    });
   }
+
+  return (
+    <div className="d-flex flex-column h-100">
+      <div className="loginBG" />
+      <h1 className="vybe text-center">VYBE</h1>
+      <div className="container h-100">
+        <Row className=" h-100 row align-items-center">
+          <Col size="sm-7">
+            <h3 className="testColor">Intro</h3>
+          </Col>
+          <Col size="sm-5">
+            {(!isAuthenticated) && (
+              <h3
+                className="testColor"
+                onClick={() => loginWithRedirect({})}
+              >
+                Login Block</h3>)}
+                {(isAuthenticated) && (
+              <h3
+                className="testColor"
+                onClick={() => logoutWithRedirect({})}
+              >
+                Logout Block</h3>)}
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 }
 export default Login;
--- client2/src/index.js
+++ client2/src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from "./serviceWorker";
-
+import history from "./utils/history";
+import { Auth0Provider } from "./react-auth0-spa";
 import './index.css';
 import App from './App/App';
 
-render((
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-), document.getElementById('root'));
 
-serviceWorker.register();
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+
+render(
+  <Auth0Provider
+    domain={'still-frost-0676.auth0.com'}
+    client_id={'h3vADtvG4mOWGXga4Wsc8HWVLZD2TQG5'}
+    redirect_uri={window.location.origin+"/home"}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();

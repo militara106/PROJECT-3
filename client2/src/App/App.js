04aacd7f8d710f1540cc2efbe896bccbd13f320b
Alex <militara106@gmail.com>
2019-12-03 00:55:18 -0800

    mobile spacing fix, changed verbage

--- client2/src/App/App.js
+++ client2/src/App/App.js
@@ -14,7 +14,8 @@ const App = () => {
   const loadingStyle = {
     position: "absolute",
     top: "50%",
-    left: "50%"
+    left: "50%",
+    margin: "-60px"
   }
 
   if (loading) {
--- client2/src/App/components/Verbage/index.js
+++ client2/src/App/components/Verbage/index.js
@@ -2,18 +2,14 @@ import React, { Component } from "react";
 import TextLoop from "react-text-loop";
 
 class Verbage extends Component {
-  verbage = ["Beat", "Love", "VYBE"];
+  verbage = ["Rock","Chill","PLUR","Club"];
 
   render() {
     return (
       <div id="verbContainer">
         <TextLoop>
           {this.verbage.map(element => {
-            if (element === "VYBE") {
-              return <span className="lightestFont">{element}</span>
-            } else {
               return <span>{element}</span>;
-            }
           })}
         </TextLoop>
       </div>

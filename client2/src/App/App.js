aac506c0216faea0d434004f0a9cf568068f7cb8
Alex <militara106@gmail.com>
2019-12-03 02:29:04 -0800

    attempt fix on audio controls not showing up, loading removed

--- client2/src/App/App.js
+++ client2/src/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
-import Loading from "./components/Loading";
+// import Loading from "./components/Loading";
 import Login from './pages/Login';
 import SpotifyHome from './pages/SpotifyHome';
 import history from "../utils/history";
@@ -11,16 +11,16 @@ import { useAuth0 } from "../react-auth0-spa";
 const App = () => {
   const { loading, user } = useAuth0();
 
-  const loadingStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    margin: "-60px"
-  }
+  // const loadingStyle = {
+  //   position: "absolute",
+  //   top: "50%",
+  //   left: "50%",
+  //   margin: "-60px"
+  // }
 
-  if (loading) {
-    return (<div style={loadingStyle}><Loading /></div>);
-  }
+  // if (loading) {
+  //   return (<div style={loadingStyle}><Loading /></div>);
+  // }
   return (
     <Router history={history}>
       {console.log(user)}

a7744768db0305187c62a28ef0fec4d4de8dc1fe
tylerhipke <tylerhipke@gmail.com>
2019-12-04 23:17:41 -0800

    add lyrics as string literals

--- client2/src/App/components/Lyrics/index.js
+++ client2/src/App/components/Lyrics/index.js
@@ -1,5 +1,256 @@
 import React, { Component } from "react";
 
+const lyrics = [
+  `Intro
+  (Yeh-yeh-yeh-yeh yeh-yeh-yeh-yeh)
+  Si te vas yo quiero saber si tú te vas
+  Mami cuando tú quieras cuando tú quieras yeh yeh
+  
+  Coro
+  Vete eh-eh-eh
+  Nadie te está aguantando y la puerta está abierta eh
+  No te preocupe por nosotro do nuestra historia ya está muerta
+  Espero que seas feliz y que te diviertas eh
+  Pero pa acá no vuelvas no-no-no-no
+  Vete eh-eh-eh
+  Nadie te está aguantando y la puerta está abierta eh
+  No te preocupe por nosotro do nuestra historia ya está muerta
+  Espero que seas feliz y que te diviertas eh
+  Pero pa acá no vuelvas no-no-no-no
+  
+  Verso
+  Ey dime qué esperas
+  Baby te quiero pero afuera
+  Ere alguien por dentro y otra por fuera
+  Y ya no siento nada cuando te encuera
+  Dame-dame banda eh
+  En mi corazón ya tú no ere la que manda
+  Se acabó por ti ya no siento nada
+  De nuestra serie ya no salen temporada
+  Así que vete lejo dile al diablo que te envíe el pin
+  Hace tiempo que no somo un team
+  Pal carajo nuestro aniversario y San Valentín
+  Ya no hay más Christian Lou ni los traje en satén
+  Síguelo que está verde eh
+  Que tiene la culpa es lo que te muerde
+  Quédate con el perro pa que de mí te acuerde
+  Y piense en to lo que te pierde
+  
+  Puente
+  Pero te deseo suerte ahora soy má fuerte
+  Gracia a to lo que me hiciste eh
+  Tú nunca me quisiste eh
+  No sé por qué me insiste
+  Pero te deseo suerte ahora soy má fuerte
+  Gracia a to lo que me hiciste eh
+  Tú nunca me quisiste eh
+  No sé por qué me insiste
+  
+  Coro
+  Vete eh-eh-eh
+  Nadie te está aguantando y la puerta está abierta eh
+  No te preocupe por nosotro do nuestra historia ya está muerta
+  Espero que seas feliz y que te diviertas eh
+  Pero pa acá no vuelvas no-no-no-no
+  Vete eh-eh-eh
+  Nadie te está aguantando y la puerta está abierta eh
+  No te preocupe por nosotro do nuestra historia ya está muerta
+  Espero que seas feliz y que te diviertas eh
+  Pero pa acá no vuelvas no-no-no-no
+  
+  Outro
+  Hazen`,
+  `Intro: Big Sean
+  Yeah, what happens when it's too good to throw away?
+  You either let it go bad or go away
+  (I need to pray)
+  
+  Pre-Chorus: Big Sean & Ty Dolla $ign
+  Take my time (Part two), take my time, I'ma take my time
+  Na-na-na-na-na-na-na (Look)
+  Na-na-na-na-na-na-na (Uh)
+  
+  Chorus: Big Sean
+  I'm single again (Single again)
+  Told her we'll be better off friends (Uh)
+  Baby, I should just focus on me (On me)
+  And slow down, don't rush romance
+  I'm single again (Single again)
+  Told 'em that we better off friends (Off friends)
+  Blaming you is just so easy (Woah)
+  But maybe the problem's me
+  I'm single again (Single again)
+  Guess that's just the way it's gon' be (Gon' be)
+  Maybe I should spend this time on me (On me)
+  Maybe I should spend this cash on me (On me)
+  
+  Verse 1: Big Sean
+  Instead of blaming all my old girls when I leave (What?)
+  Instead of calling up these fake hoes that I don't need (What?)
+  Old relationships turn to new déjà vus
+  Got me feeling like, "I don't fuck with you"
+  Oh nah, nah, that's the old me
+  You fucking with the new me (Yeah)
+  Honestly, all the disrespect had damn near ruined me (Damn near)
+  All the trust gone, seems like all you say is, "Prove it to me" (Straight)
+  And you know I suck at lying, don't, don't do it to me (Don't)
+  Maybe 'cause my mom had never worked it out with my dad (Damn)
+  Maybe 'cause he had insecurities and she had 'em back (Uh-huh)
+  Maybe 'cause single parent love was all I ever had
+  Who knows? (Who knows?)
+  
+  Chorus: Big Sean & Jhené Aiko, Big Sean
+  I'm single again (Single again)
+  Told her we'll be better off friends (Off friends)
+  Maybe I should just focus on me (On me)
+  And slow down, don't rush romance
+  I'm single again (Single again)
+  Told 'em that we better off friends (Off friends)
+  Blaming you is just so easy (Uh-huh)
+  But maybe the problem's me (Straight up)
+  I'm single again (Single again)
+  Guess that's just the way it's gon' be (Yeah, yeah)
+  Maybe I should spend this time on me (On me)
+  Maybe I should spend this cash on me
+  
+  Post-Chorus: Big Sean & Jhené Aiko
+  Take my time, take my time, I'ma take my time
+  Take my time, I'ma take my time, yeah
+  Even if I'm late, I'ma take my time
+  
+  Verse 2: Big Sean & Ty Dolla $ign
+  What have you done for yourself?
+  What have you done for your mental health? (Yeah, yeah)
+  I even tried the drugs and they didn't help
+  Short term fix, that breaks everything else (Oh, oh)
+  And my bros keep throwing me these random girls in stallions
+  I need the best advice, went across the street to Khaled (Oh)
+  He said the major key is to be strong on your own
+  Lose your other half, you're never off balance (Oh)
+  Lookin' for the love in somebody else
+  That I probably should've gave to myself
+  Cycles I need to break in myself
+  If I'm alone, I'm second to no one else, so (Yeah)
+  
+  Chorus: Big Sean & Ty Dolla $ign
+  I'm single again (Oh)
+  Told her we'll be better off friends (Better off friends)
+  Maybe I should just focus on me (On me)
+  And slow down, don't rush romance
+  I'm single again
+  Told 'em that we better off friends (Oh, yeah)
+  Blaming you is just so easy (Oh)
+  But maybe the problem's me (Ooh)
+  I'm single again
+  Guess that's just the way it's gon' be (Way)
+  Maybe I should spend this time on me (Way)
+  Maybe I should spend this cash on me (Ooh)
+  
+  Post-Chorus: Big Sean
+  Take my time, take my time, I'ma take my time, yeah
+  Take my time, I'ma take my time, yeah
+  Even if I'm late, I'ma take my time
+  
+  Outro: Big Sean & Ty Dolla $ign
+  Yeah
+  Stay on this grind
+  (Oh, oh, ayy)
+  (Dolla $ign)
+  (Oh)`,
+  `[Intro: Soyeon & Becky G]
+  원하는 게 얼굴에 보여
+  I'm trouble and you're wa-wa-wanting it
+  Yeah, yeah
+  
+  [Verse 1: Thutmose]
+  Movin' too fast, life is movin' in slow-mo
+  I'm a god, better ask if you don't know
+  Homie, better put your pride aside (Aside)
+  I'm a Benz and you're more like a Volvo
+  Your best stuff looks like my worst, synapses fire and burst
+  Got the whole crew with me, 'bout to deal damage
+  You know we ain't average (Oh yeah)
+  I ain't gonna say this again
+  But this is my time, better look in my eyes
+  I'm a genius in disguise, wear my heart on my sleeve
+  And you forced to oblige to a king in his prime (Yeah, yeah)
+  Everybody get in line, sit back, watch the stars align
+  I finesse like my life on the line
+  Was a diamond in the rough and now I shine (Woo)
+  
+  [Pre-Chorus: Becky G]
+  Ay, no one can stop us, they'll try, but they won't
+  Ay, nada nos puede parar, oh, no
+  
+  [Chorus: Keke Palmer]
+  We're wide awake now, our eyes are wide open
+  We're runnin' this world, we're keepin' it turnin'
+  We're livin' like giants, yeah, giants
+  We're bigger than giants, we're giants, oh
+  
+  [Post-Chorus: Becky G]
+  Sleepin' giants, sleepin', sleepin' giants (Yah)
+  
+  [Verse 2: Becky G & Soyeon]
+  Shuttin' it down, been underground
+  Now my people all up in this place (Place)
+  Spinnin' this thing, gold on my ring, royalty up in my veins (Veins)
+  I'm loco, think I'm so cold, I stay stuntin', ain't no goin' back
+  Now they ask where QiQi's at, I'm like you can't hang with that
+  쏟아져 빛이 멀게 돼 눈이
+  멈추진 않아 once we go
+  날카로운 날로 뭐든 베어
+  빨리 달려 넌! 못.따.라. 와~
+  True Damage, we do it, True Damage 보여줄게 뭐든 쉽게
+  이기든지든, my crew make the beat drop
+  
+  [Pre-Chorus: Becky G]
+  Ay, no one can stop us, they'll try, but they won't (They won't)
+  Ay, nada nos puede parar, oh, no
+  
+  [Chorus: Keke Palmer, Becky G & Both]
+  We're wide awake now, our eyes are wide open
+  We're runnin' this world, we're keepin' it turnin'
+  We're livin' like giants (Yeah), yeah, giants
+  We're bigger than giants (Yeah), we're giants
+  You thought that we were weak, but we comin' right back (Right back)
+  This time, you're gonna see how we do it like that (Yeah)
+  We're livin' like giants, yeah, giants (Yeah)
+  We're bigger than giants, we're giants, oh
+  
+  [Post-Chorus: Becky G]
+  Sleepin' giants, sleepin', sleepin' giants
+  
+  [Verse 3: DUCKWRTH, Soyeon & Keke Palmer]
+  Mm, I keep on shattering time
+  I might just leave you behind
+  Homie, you stuck on rewind (Yeah, yeah, yuh)
+  You should quit sleepin' on mines (Woo)
+  You got your back on recline
+  When I damage your delicate spine
+  Please don't act all shy and surprised (누구)
+  They be like (뭐), they be like
+  What you mean? (Uh) What is your winning scheme? (Woo)
+  I got a giant team (That's what?), big as my self esteem (Big)
+  Run it back, run it back, runnin' man (Run it)
+  They think they go hard, I'm like come again (Sheesh)
+  You run out of time when I'm bustin' in, while you stumblin'
+  I got this blade in my hand for your punishment (Oh, woo)
+  
+  [Chorus: Keke Palmer, Becky G, Both & Thutmose]
+  We're wide awake now, our eyes are wide open
+  We're runnin' this world, we're keepin' it turnin'
+  We're livin' like giants (Giants), yeah, giants (Yeah)
+  We're bigger than giants (Woo), we're giants (Yeah)
+  You thought that we were weak, but we comin' right back (Right back)
+  This time, you're gonna see how we do it like that (Yeah)
+  We're livin' like giants (Giants), yeah, giants (Hey)
+  We're bigger than giants (Hey), we're giants, oh
+  
+  [Post-Chorus: Becky G & All]
+  Sleepin' giants, sleepin', sleepin' giants`
+]
+
 export default class Lyrics extends Component {
   state = {
     lyrics: ""
